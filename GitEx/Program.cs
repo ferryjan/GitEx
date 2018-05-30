8955a997805584db3d11c7f3b191e77f0fda994b
Jerry Fan <jerry_swe@hotmail.com>
2018-05-30 09:56:54 +0200

    Add cw "Hello Again"

--- GitEx/Program.cs
+++ GitEx/Program.cs
@@ -11,6 +11,7 @@ namespace GitEx
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello again!");
         }
     }
 }
